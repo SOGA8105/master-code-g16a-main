dbbf52112f8e0c972736681be82f56b15353a7c5
Angel Solis <asolisgaribay@gmail.com>
2022-05-10 21:34:22 -0500

    .

--- 02-Intro_a_JS/07_Scope/js/main.js
+++ 02-Intro_a_JS/07_Scope/js/main.js
@@ -4,11 +4,28 @@
 //     var color = 'rojo';
 //     console.log(color);
 
-//     var listaDeColores = 'Mi color favorito es: ' + color;
-//     console.log(listaDeColores)
+//     var listaDeColores = 'Mi colore favorito es: ' + color;
+//     console.log(listaDeColores);
 // }
-
-// pinturas(color);
 
 // console.log(color);
 
+// pinturas(color);
+
+var edadActual = 4;
+
+function calcularDelBebe(){
+    var edadTotal = ('una operacion');
+    var suma = edadTotal + edadActual;
+
+    function dias(){
+        var diasDeNacido = 5;
+        edadActual = diasDeNacido - 8;
+        
+
+    }
+
+    console.log(edadTotal);
+}
+calcularDelBebe();
+console.log(edadTotal);
